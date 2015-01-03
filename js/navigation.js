395265d1fea48c1646998a80536c5ef021b457c2
心尘 <isxinchen@gmail.com>
2015-01-03 22:35:01 +0800

    init project with an available index page

---
+++ js/navigation.js
@@ -0,0 +1,12 @@
+$(function(){
+	// $('#parent').append('<li>xx</li>');
+	// $('#child').append('<li>xx</li>');
+	// $('.child_item_add').on('click',function(){
+	// 	$('#child').append('<li>xxxxxxxx</li>');
+	// });
+	
+	
+});
+(function(){
+
+})();
---
+++ navigation.php
@@ -0,0 +1,15 @@
+<?php
+
+class Category{
+	var $c_id
+	var $c_name;
+	var $c_index;
+}
+
+class Item{
+	var $i_id;
+	var $i_name;
+	var $i_url;
+	var $i_index;
+}
+?>
