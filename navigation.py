be47df95a30047ddcb5b2804dc66593062d0ebf6
xin_chen <chenxin@syberos.com>
2015-02-09 18:20:38 +0800

    none

--- navigation.py
+++ navigation.py
@@ -1,8 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# coding=utf-8
 
 #This is a python script for my navigation
 
+#spawn-fcgi -d /home/chenxin/work/git/navigation/ -f /home/chenxin/work/git/navigation/navigation.py -a 127.0.0.1 -p 9002
+
+
+import web
+
 groups = []
+urls = ("/", "hello",
+	"/api/addgroup(.+)", "addgroup",
+	"/api/delgroup(.+)", "delgroup",
+	"/api/editgroup(.+)", "editgroup",
+	"/api/additem(.+)", "additem",
+	"/api/delitem(.+)", "edititem",
+	"/api/editgroup(.+)", "editgroup")
+app = web.application(urls, globals())
 
 class group:
 	g_id = -1
@@ -36,11 +50,70 @@ class item:
 	def set_name(self, name):
 		self.name = name
 
+class hello:
+    def GET(self):
+        return 'Hello, world!'
+    
+    if __name__ == "__main__":
+        web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+        app.run()
+
+class addgroup(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "add group"
+
+class delgroup(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "del group"
+
+class editgroup(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "edit group"
+
+class additem(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "add item"
+
+class delitem(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "del item"
+
+class edititem(object):
+	"""docstring for ClassName"""
+	# def __init__(self, arg):
+		# super(ClassName, self).__init__()
+		# self.arg = arg
+	def GET(self):
+		return "edit item"
+
 def info():
 	for group in groups:
 		print group.g_id,group.g_name
 		for item in group.items:
 			print item.id,item.name,item.url
+
+def parser():
+	pass
 
 if __name__ == '__main__':
 	item1 = item(1, "qq", "http://www.qq.com")
